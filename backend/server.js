6e3607b42c627d7fd1fcac4ea1f3fc0c2ec7e6dc
Rizwanu321 <Rizwanurahman321@gmail.com>
2023-09-14 20:29:55 +0530

    final code

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import morgan from 'morgan'
+import cors from "cors";
+import path from 'path'
+import authRoutes from './routes/authRoute.js'
+import productRoutes from './routes/productRoutes.js'
+
+
+//configure ev
+dotenv.config()
+
+//database config
+connectDB()
+
+//rest object
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+//routes
+app.use('/api/v1/auth',authRoutes)
+app.use("/api/v1/product", productRoutes);
+
+//rest api
+app.use('/', (req, res)=> {
+  res.send("welcome")
+})
+
+//port
+const PORT = process.env.PORT || 8080
+
+//run listen
+app.listen(PORT, ()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`)
+})
